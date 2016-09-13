690e10eb73057b8a94ad60b1a2bcda70a162994b
Tim V <vincent3@purdue.edu>
2016-09-13 00:16:39 -0400

    Added demo and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# IEEE basic Python Calculator
+
+##Setup
+After using git clone to download the repo, we need to cd into our new directory
+
+##Work that needs to be done
+Currently our multiplication method (mul) is unimplemented. Create a branch to fix the code, then create a pull request on your github repo! 
+
---
+++ calc.py
@@ -0,0 +1,25 @@
+# Mark: The basic operations of our calculator
+def add(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def mul(a, b):
+	return 0 #TODO: Your code here
+
+def div(a, b):
+	return a / b
+
+def mod(a, b):
+	return a % b
+
+#Mark: Our main method which will run when we call the program
+def main():
+	a = 66	#First number
+	b = 33	#Second number
+	result = div(a,b)	# First divided by second
+	print("The result is %.2f" % result)
+
+if __name__ == "__main__":
+	main()
